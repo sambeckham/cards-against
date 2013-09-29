bc7de86952dce246dc7d21a7af96884b0612ce39
Sam Beckham <sam@sambeckhamdesign.com>
2013-09-29 17:47:56 +0100

    Added in angular and got the basics of socket.io running, needs clean up and further integration though

--- index.js
+++ index.js
@@ -1,33 +1,69 @@
-var express = require('express');
+var express = require('express'),
+  app = express(),
+  server = require('http').createServer(app),
+  io = require('socket.io').listen(server);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/app/index.html');
+});
+
+app.use(express.static(__dirname + '/app'));
+
+io.sockets.on('connection', function (socket) {
+  var room = 'room1';
+  socket.room = room;
+  socket.join(room);
+  socket.emit('message', { message: 'welcome to the chat, you have connected to ' + room });
+  socket.on('send', function(data) {
+    io.sockets.in(socket.room).emit('message', data);
+  });
+  socket.on('switchRoom', function(data) {
+    var newRoom = data.room;
+    console.log('switching to ' + newRoom);
+        socket.leave(socket.room);
+        socket.join(newRoom);
+        socket.emit('message', { message: 'welcome to the chat, you have connected to ' + newRoom });
+        socket.room = newRoom;
+  });
+});
+
+exports = module.exports = server;
+// delegates user() function
+exports.use = function() {
+  app.use.apply(app, arguments);
+};
+
+/* var express = require('express');
 var app = express();
 var port = 3700;
 
-app.set('views', __dirname + '/tpl');
-app.set('view engine', "jade");
-app.engine('jade', require('jade').__express);
 app.get("/", function(req, res){
-	res.render("page");
+	res.send("page");
 });
 
 var rooms = ['room1', 'room2'];
 
-app.use(express.static(__dirname + '/public'));
-var io = require('socket.io').listen(app.listen(port));
+// app.use(express.static(__dirname + '/public'));
+// var io = require('socket.io').listen(app.listen(port));
 
-io.sockets.on('connection', function(socket) {
-	var room = 'room1';
-	socket.room = room;
-	socket.join(room);
-	socket.emit('message', { message: 'welcome to the chat, you have connected to ' + room });
-	socket.on('send', function(data) {
-		io.sockets.in(socket.room).emit('message', data);
-	});
-	socket.on('switchRoom', function(data) {
-		var newRoom = data.room;
-		console.log('switching to ' + newRoom);
-		socket.leave(socket.room);
-		socket.join(newRoom);
-		socket.emit('message', { message: 'welcome to the chat, you have connected to ' + newRoom });
-		socket.room = newRoom;
-	});
-})
+// io.sockets.on('connection', function(socket) {
+//	var room = 'room1';
+//	socket.room = room;
+//	socket.join(room);
+//	socket.emit('message', { message: 'welcome to the chat, you have connected to ' + room });
+//	socket.on('send', function(data) {
+//		io.sockets.in(socket.room).emit('message', data);
+//	});
+//	socket.on('switchRoom', function(data) {
+//		var newRoom = data.room;
+//		console.log('switching to ' + newRoom);
+//		socket.leave(socket.room);
+//		socket.join(newRoom);
+//		socket.emit('message', { message: 'welcome to the chat, you have connected to ' + newRoom });
+//		socket.room = newRoom;
+//	});
+// });
+
+app.listen(port);
+console.log('server runnng and is listening n port ' + port);
+*/
