50cba03bd909deb86724c6c39e43c9b361331df8
Sam Beckham <sam@sambeckhamdesign.com>
2014-02-16 23:28:00 +0000

    Re-structured the SCSS files to make them easier to edit.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,7 +4,7 @@ angular.module('cardsAgainstApp')
   .controller('MainCtrl', function ($scope, $location) {
     var code = Math.random().toString(36).slice(10);
     $scope.roomId = code;
-    $scope.ip = '10.0.4.242'; //The ip address of your machine/server
+    $scope.ip = 'localhost'; //The ip address of your machine/server
     $scope.port = '9000'; // This is set in Gruntfile.js
 
     $scope.goToChat = function() {
